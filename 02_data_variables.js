8b2e9cc6b08eec8e53cf8a64534ce3181291bedf
sakshamkr73 <146086469+sakshamkr73@users.noreply.github.com>
2023-09-26 11:55:52 +0000

    practicing data variables

---
+++ 02_data_variables.js
@@ -0,0 +1,25 @@
+let name1="saksham kumar"
+const productId= 90
+var productseries="37379"
+people=55
+
+// productId=45
+
+console.log(name1);
+console.log(productId);
+console.log(productseries);
+console.log(people);
+
+console.log(typeof name1);
+console.log(typeof productId);
+console.log(typeof productseries);
+console.log(typeof people);
+
+
+let stringproductId=String(people)
+let numberproductseries=Number(productseries)
+
+console.log(stringproductId+"u");
+console.log(numberproductseries);
+console.log(typeof stringproductId);
+console.log(typeof numberproductseries);
