be77811a647d4bc5cc0585727277ccd97a3ef876
sakshamkr73 <146086469+sakshamkr73@users.noreply.github.com>
2023-09-28 06:14:10 +0000

    Operations on same or different data types

---
+++ codes/04_some_operation.js
@@ -0,0 +1,28 @@
+let str1 = "hello"
+let str2 = " duniya"
+console.log(str1 + str2);
+
+console.log(1 + 2);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + "2");
+console.log("1" + "2" + 3);
+console.log("1" + 2 + 3);
+console.log(1 + 3 + "2");
+console.log(1 + 3 + "2" + 3 + null);
+console.log(1 + 3 + null);
+console.log(null + 1 + 3);
+console.log(1 + 3 + undefined);
+console.log(null + "1" + 2 + 3);
+
+
+console.log(true);
+console.log(+true);
+console.log(-true);
+// console.log(true+); error
+console.log(+false);
+console.log(-false);
+console.log(+"");
+console.log(-"");
+console.log(+"hkljh");
+console.log(-"hkljh");
