991e51f1ed77a5699c075608298e09cc127c5585
sakshamkr73 <146086469+sakshamkr73@users.noreply.github.com>
2023-09-28 06:13:28 +0000

    More information about data types

---
+++ codes/03_data_types.js
@@ -0,0 +1,25 @@
+// number => 2^53
+// BigInt
+// boolean => true/false
+// null => standalone value(object)
+// undefined => undefined
+// Symbol=> unique
+// string=>""
+// Object 
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+// let score="33"
+let score = "33abs"
+let valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);//NaN
+
+// "33" => 33
+// "33aha => NaN"
+// 1 => true, 0=> false
+// "gjha" =>true, "" =>false
+
+
+
