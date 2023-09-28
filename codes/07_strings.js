919b7ceae9ea6290f2b0e1ad9374813f6546817b
sakshamkr73 <146086469+sakshamkr73@users.noreply.github.com>
2023-09-28 07:28:25 +0000

    Practicing some string built-in functions

---
+++ codes/07_strings.js
@@ -0,0 +1,32 @@
+const name = "saksham"
+let age = 16
+console.log("age of " + name + " is " + age)
+
+console.log(`Age of ${name} is ${age}`)
+
+const nametow=new String('Saksham_Kumar_1278')
+
+console.log(nametow);
+console.log(nametow[0]);
+console.log(nametow[4]);
+console.log(nametow.__proto__);
+
+console.log(nametow.length);
+console.log(nametow.anchor('hkajds'));
+console.log(nametow.at(6));
+console.log(nametow.big());
+console.log(nametow.blink());
+console.log(nametow.bold());
+console.log(nametow.charAt(4));//char
+console.log(nametow.charCodeAt(4));//unicode
+console.log(nametow.codePointAt(17));//unicode
+console.log(nametow.fixed());
+console.log(nametow.includes('Saksham'));
+console.log(nametow.includes('Sakshamw'));
+console.log(nametow.indexOf('j'));
+console.log(nametow.indexOf('S'));
+console.log(nametow.indexOf('Kumar'));
+console.log(nametow.indexOf('Kumar'));
+console.log(nametow.split('_'));
+console.log(nametow.trim());
+console.log(nametow.substring(3,7));
