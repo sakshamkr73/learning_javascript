457eda67c7d65a30819455483e34d90ca156595b
sakshamkr73 <146086469+sakshamkr73@users.noreply.github.com>
2023-09-28 06:16:08 +0000

    Comparision operations on same or different data types

---
+++ codes/05_comparision.js
@@ -0,0 +1,29 @@
+console.log("2" > 1);
+console.log("2" < 1);
+console.log("02" > 1);
+console.log("02" == 1);
+console.log("01" == 1);
+console.log("01" <= 1);
+console.log("01" >= 1);
+
+console.log(null > 1);
+console.log(null < 1);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null > "0");
+console.log(null > "1");
+console.log(null < "1");
+console.log(null == "1");
+console.log(null == 1);
+console.log(null == "0");
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+//=== => it also check data types
+console.log(2 === 2);
+console.log(2 === 1);
+console.log("2" === 1);
+console.log("2" === 2);
+console.log(null === 1);
+console.log(null === 0);
